28b79a4e0117fe37bf367dc16eb075c8adf5a6f9
stanley <stanley.alucheri@spinmobile.co.ke>
2023-01-28 13:39:51 +0300

    state service testing

--- core/tests/test_services.py
+++ core/tests/test_services.py
@@ -21,4 +21,27 @@ class TestStateService(object):
 		"""
 		mixer.blend('core.State', name="Active")
 		state = StateService().get(name="Active")
+		assert state is not None and state.name == "Active", 'Should have a State object'
+
+	def test_filter(self):
+		"""
+		Test State filter service
+		"""
+		mixer.cycle(4).blend('core.State', name="Active")
+		state = StateService().filter(name="Active")
+		assert len(state) == 4, 'Should have 4 State objects'
+
+	def test_create(self):
+		"""
+		Test StateService create() method
+		"""
+		state = StateService().create(name="Active")
 		assert state is not None, 'Should have a State object'
+
+	def test_update(self):
+		"""
+		Test StateService update() method
+		"""
+		status = mixer.blend('core.State', name="Active")
+		state = StateService().update(status.id, name="Completed")
+		assert state is not None and state.name == "Completed",  'Should have an updated  State object'
